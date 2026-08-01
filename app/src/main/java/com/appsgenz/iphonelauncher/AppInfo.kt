b52529f78854042b63c4a34932c18aaa480d234c
jeie17655-web <jeie17655@gmail.com>
2026-07-31 21:43:06 -0300

    Create AppInfo.kt

---
+++ app/src/main/java/com/appsgenz/iphonelauncher/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.appsgenz.iphonelauncher
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val label: String,
+    val packageName: String,
+    val icon: Drawable
+)
